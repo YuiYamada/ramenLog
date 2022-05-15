49a30f8e0c8fd8ed1ab7d6d0ca3cc291be9b9f36
John Doe <johndoe@example.com>
2022-05-15 22:40:48 +0900

    遷移先でデータ取得

--- js/index2.js
+++ js/index2.js
@@ -1,4 +1,4 @@
-$('#check').click(function input(){
+/*$('#check').click(function input(){
     var placeName = $('input[name = "place name"]:checked').val();
     var genreName = $('input[name = "genre name"]:checked').val();
 
@@ -6,4 +6,21 @@ $('#check').click(function input(){
 
     $("#test").html(placeName + genreName)
     return {placeName:placeName, genreName:genreName}
-})
+}) */
+
+
+function getUrlParam(param){
+    var pageUrl = window.location.search.substring(1);
+    var urlVar = pageUrl.split('&');
+    for (var i = 0; i < urlVar.length; i++){
+        var paramName = urlVar[i].split('=');
+        if (paramName[0] == param) {
+            return decodeURI(paramName[1]);
+        }
+    }
+}
+
+var placeName = getUrlParam("place+name");
+var genreName = getUrlParam("genre+name");
+$("#test2").html(placeName + "周辺の" + genreName + "ランキング")
+

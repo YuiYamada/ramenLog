53e2d4c0bec552c5c74dfb80feddc2d0c83a8b54
John Doe <johndoe@example.com>
2022-05-25 12:08:34 +0900

    絞り込みメニュー追加　少し挙動ヘン

--- js/index2.js
+++ js/index2.js
@@ -11,21 +11,47 @@
 function getUrlParam(param){
     var pageUrl = window.location.search.substring(1);
     var urlVar = pageUrl.split('&');
-    for (var i = 0; i < urlVar.length; i++){
-        var paramName = urlVar[i].split('=');
-        if (paramName[0] == param) {
-            return decodeURI(paramName[1]);
+    if(urlVar == ''){
+        return "";
+    }else{
+        for (var i = 0; i < urlVar.length; i++){
+            var paramName = urlVar[i].split('=');
+            if (paramName[0] == param) {
+                return decodeURI(paramName[1]);
+            }
         }
     }
+    
 }
 
 var placeName = getUrlParam("place+name");
 var genreName = getUrlParam("genre+name");
 $("#test2").html(placeName + "人気" + genreName +"ラーメン店！")
 
+//初期化ボタン
+$('#delete').click(function(){
+    $.getJSON("../json/shopData3.json").done(function(json){
+        let json_data = JSON.stringify(json);
+        localStorage.setItem('data', json_data);
+        makeList(json,placeName,genreName)
+    })
+});
+
 //読み込み時リスト作成
 $(function(){
-    makeList(loadLocal(),getUrlParam("place+name"),getUrlParam("genre+name"));
+
+    $('[name *= "name"]').click(function() {
+		var selectPlace = $('input:radio[name = "place name"]:checked').val();
+        var selectGenre = $('input:radio[name = "genre name"]:checked').val();
+        var selectFav = $('input:radio[name = "fav name"]:checked').val();
+        if(selectFav == 0){
+            makeList(loadLocal(),selectPlace,selectGenre);
+        }else{
+            makeFavList(loadLocal(),selectPlace,selectGenre);
+        }
+	});
+    makeList(loadLocal(),placeName,genreName);
+    
 })
 
 //リスト作成関数
@@ -69,6 +95,46 @@ function makeList(json,placeName,genreName){
     document.getElementById("shopList").innerHTML = resultHTML;
 }
 
+//お気に入りリスト作成
+function makeFavList(json,placeName,genreName){
+    $("#shopList").empty();
+    var resultHTML = "<ul>";
+    var imgWidth = "300";
+    var imgHeight = "150";
+    for (var i = 0; i < json.length; i++) {
+        
+        var rating = json[i].rating;
+        var shopName = json[i].shopName;
+        var genre = json[i].genre;
+        var place = json[i].place;
+        var imgURL = json[i].shopImgURL;
+        var id = json[i].id;
+        var check = json[i].checked;
+        var detailUrl = "../html/store_detail.html" + "?" + "shopName" + "=" +shopName;
+        //if(rating == undefined) rating = "---";
+        
+        if((placeName === place || placeName === "") && (genreName === genre || genreName === "") && check == 1){
+            //表示内容（評価＋名称）
+            var content =  "<img src=" + imgURL + " " + "width=" + imgWidth + "height=" + imgHeight + ">"
+             + "  店舗名：" + shopName.link(detailUrl) + "  ★" + rating ;
+
+            if(check==0){
+                var bookMark = "<input type=" + "checkbox" + " " +"id=" + id + " name=shop>"
+            }else{
+                var bookMark = "<input type=" + "checkbox" + " " +"id=" + id + " name=shop checked>"
+            }
+            
+            
+            resultHTML += "<li>"
+            resultHTML += content;
+            resultHTML += bookMark;
+            resultHTML += "</li>";
+        }
+    }
+    resultHTML += "</ul>";
+    //結果表示
+    document.getElementById("shopList").innerHTML = resultHTML;
+}
 
 //ローカルストレージにデータあればロード　無ければアップする
 function loadLocal(){
@@ -85,17 +151,15 @@ function loadLocal(){
     }
 }
 
+
 $(function(){
-    $('[name="shop"]').change(function(){
+    $('input[name="shop"]').change(function(){
         var json = loadLocal();
-            console.log("s");
-            $('input:checked').each(function() {
+            $('input[name="shop"]:checked').each(function() {
                 var r = $(this).attr("id");
-                console.log(r);
                 json[r].checked = 1;
-                console.log(json[r]);
             })
-            $('input:not(:checked)').each(function() {
+            $('input[name="shop"]:not(:checked)').each(function() {
                 var r = $(this).attr("id");
                 json[r].checked = 0;
             })

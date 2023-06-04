abbbadeedf8e136b9b70782bc39d341e139ef2bf
Yamada-Yuito <yui-yamada@agilecore.co.jp>
2023-06-04 12:28:06 +0900

    いらないファイル削除

--- js/index.js
+++ js/index.js
@@ -1,100 +1,270 @@
-//window.onload = function main() {
-    //let table = document.getElementById('storeList');
-
-     //これが追加したいのが二項目じゃなかったら?
-     //どうしたらまとめられるか考えてみましょう。
-    //let newRow = table.insertRow();
-    //let newCell = newRow.insertCell();
-    //let anchor = document.createElement('a');
-     //ここで渡す値はnameで良いのか?
-     //セキュリティ的にはどうなのか考えてみましょう。
-    //anchor.href = './store_detail.html?name=コロニアルラウンジ';
-    //anchor.innerText = 'コロニアルラウンジ';
-    //newCell.appendChild(anchor);
-
-    //newRow = table.insertRow();
-    //newCell = newRow.insertCell();
-    //anchor = document.createElement('a');
-    //anchor.href = './store_detail.html?name=BURGER\'S BASE';
-    //anchor.innerText = 'BURGER\'S BASE';
-    //newCell.appendChild(anchor);
-//};
-
-function clickSearch(){
-    var search_catch = document.getElementById("tx").value;
-    placeNameResult = search_catch + "周辺の人気飲食店";
-    document.getElementById('placeName').innerHTML = placeNameResult;
-
-    //テスト配列
-    var placesList = [
-        {rating:4.8, shopName:"AAA中華"},
-        {rating:4.7, shopName:"BBB焼き肉"},
-        {rating:4.6, shopName:"CCC寿司"},
-        {rating:4.5, shopName:"DDD定食"},
-        {rating:4.4, shopName:"EEEピザ"}
-    ]
-    
-    //placesList配列をループして、
-    //結果表示のHTMLタグを組み立てる
-    var resultHTML = "<table border = 1>";
-
-    for(var k = 0; k < placesList.length; k++){
-        var rating = placesList[k].rating;
-        var shopName = placesList[k].shopName;
-        var ranking = k+1;
-        var content = "<th>"+ ranking +"位"+ "</th>"+"<th>"+"[" + rating + "]"+"</th>" +"<td>"+ 
-        "<a href=https://www.google.co.jp/maps/place/%E3%83%9E%E3%82%AF%E3%83%89%E3%83%8A%E3%83%AB%E3%83%89+%EF%BC%AA%EF%BC%B2%E6%96%B0%E5%AE%BF%E5%8D%97%E5%8F%A3%E5%BA%97/@35.6902648,139.70088,17z/data=!4m5!3m4!1s0x60188cda95783253:0x932692b07c5d5867!8m2!3d35.6893158!4d139.7025099?hl=ja>"
-         +shopName+"</a>" +"</td>";
-
-        resultHTML += "<tr>";
-        resultHTML += content;
-        resultHTML += "</tr>";
+//URLから値を取得する関数
+function getUrlParam(param) {
+  var pageUrl = window.location.search.substring(1);
+  var urlVar = pageUrl.split("&");
+  if (urlVar == "") {
+    return "";
+  } else {
+    for (var i = 0; i < urlVar.length; i++) {
+      var paramName = urlVar[i].split("=");
+      if (paramName[0] == param) {
+        return decodeURI(paramName[1]);
+      }
     }
-    resultHTML += "</table>";
-
-    document.getElementById('txt').innerHTML = resultHTML;
+  }
 }
-    //placesList配列をループして、
-    //結果表示のHTMLタグを組み立てる
-    /*var resultHTML = "<ol>";
-    for (var i = 0; i < placesList.length; i++) {
 
-        place = placesList[i];
-        
-        //ratingがないのものは「---」に表示変更
-        var rating = place.rating;
-        //if(rating == undefined) rating = "---";
-        
-        //表示内容（評価＋名称）
-        var content = "【" + rating + "】 " + place.shopName;
-        
-        resultHTML += "<li>";
-        resultHTML += content;
-        resultHTML += "</li>";
+//変数へ場所とジャンルの名前をいれる
+var placeName = getUrlParam("place+name");
+var genreName = getUrlParam("genre+name");
+
+//絞り込みメニューを選択したとき値を取得　HTMLへ入力
+var selectPlace = $('input:radio[name = "place name"]:checked').val();
+var selectGenre = $('input:radio[name = "genre name"]:checked').val();
+$("#test2").html(selectPlace + "人気" + selectGenre + "ラーメン店！");
+
+//お気に入りしたものの初期化ボタン
+$("#delete").click(function () {
+  $.getJSON("../json/shopData3.json").done(function (json) {
+    let json_data = JSON.stringify(json);
+    localStorage.setItem("data", json_data);
+    makeList(json, placeName, genreName);
+  });
+});
+
+//絞り込みメニュークリック時リスト作成
+$(function () {
+  $('[name *= "name"]').click(function () {
+    var selectPlace = $('input:radio[name = "place name"]:checked').val();
+    var selectGenre = $('input:radio[name = "genre name"]:checked').val();
+    var selectFav = $('input:radio[name = "fav name"]:checked').val();
+    $("#test2").html(selectPlace + "人気" + selectGenre + "ラーメン店！");
+    if (selectFav == 0) {
+      makeList(loadLocal(), selectPlace, selectGenre);
+    } else {
+      makeFavList(loadLocal(), selectPlace, selectGenre);
     }
-    resultHTML += "</ol>";
-    //結果表示
-    document.getElementById("results").innerHTML = resultHTML;
-    */
+  });
+  makeList(loadLocal(), placeName, genreName);
+});
 
-    // 配列の長さの文だけループを回す
-    /*for (var i = 0; i < placesList.length; i++) {
-        // まず、idを表示する
-        // Htmlのdivの部分を指定する
-        var getData = document.getElementById('txt');
-        // 表示したいデータを指定する
-        var placesListrating = document.createTextNode(placesList[i].rating);
-        // 要素を指定し、その要素の子要素としてデータを表示する
-        getData.appendChild(placesListrating);
+//☆作
+function makeStar(rating) {
+  var Srating = "";
+  if (4.6 <= rating) {
+    Srating = "★★★★★　";
+  } else if (3.6 <= rating && rating <= 4.5) {
+    Srating = "★★★★☆　";
+  } else if (2.6 <= rating && rating <= 3.5) {
+    Srating = "★★★☆☆　";
+  } else if (1.6 <= rating && rating <= 2.5) {
+    Srating = "★★☆☆☆　";
+  } else if (0.6 <= rating && rating <= 1.5) {
+    Srating = "★☆☆☆☆　";
+  } else if (0 <= rating && rating <= 0.5) {
+    Srating = "☆☆☆☆☆　";
+  }
+  return Srating;
+}
 
-        // 次に、nameを表示する
-        var placesListname = document.createTextNode(placesList[i].shopName);
-        getData.appendChild(placesListname);
+//リスト作成関数
+function makeList(json, placeName, genreName) {
+  //リスト初期化
+  $("#shopList").empty();
+  var resultHTML = '<ul class="allList">';
+  var imgWidth = "200";
+  var imgHeight = "100";
+  for (var i = 0; i < json.length; i++) {
+    var rating = json[i].rating;
+    var shopName = json[i].shopName;
+    var genre = json[i].genre;
+    var place = json[i].place;
+    var imgURL = json[i].shopImgURL;
+    var id = json[i].id;
+    var check = json[i].checked;
+    var detailUrl =
+      "../html/store_detail.html" + "?" + "shopName" + "=" + shopName;
+    var star = makeStar(rating);
 
-        getData.appendChild(document.createElement('br'));
-    }*/
+    if (
+      (placeName === place || placeName === "") &&
+      (genreName === genre || genreName === "")
+    ) {
+      //表示内容（評価＋名称）
+      var content =
+        "<img src=" +
+        imgURL +
+        " " +
+        "width=" +
+        imgWidth +
+        " " +
+        "height=" +
+        imgHeight +
+        " class=img" +
+        ">" +
+        "<br>" +
+        "<h3 class='shopName'>" +
+        shopName +
+        "</h3>" +
+        "<div class='rate'>" +
+        "<div class='star'>" +
+        star +
+        "</div>" +
+        rating +
+        "</div>" +
+        "<br>";
 
-    //innerHTMLを使用して表示
-    //var txt = document.getElementById("txt");
-    //txt.innerHTML = text.join('');
-  
+      if (check == 0) {
+        var bookMark =
+          "<input type=" +
+          "checkbox" +
+          " " +
+          "id=" +
+          id +
+          " name=shop class=checkbox>";
+      } else {
+        var bookMark =
+          "<input type=" +
+          "checkbox" +
+          " " +
+          "id=" +
+          id +
+          " name=shop class=checkbox checked>";
+      }
+
+      resultHTML += '<li class="list">' + "<a href=" + detailUrl + ">";
+      resultHTML += content;
+      resultHTML +=
+        "<label class='check-box' for= " +
+        id +
+        ">" +
+        bookMark +
+        "<span class='check-text'>" +
+        "</span>" +
+        "</label>" +
+        "</a>";
+      resultHTML += "</li>";
+    }
+  }
+  resultHTML += "</ul>";
+  //結果表示
+  document.getElementById("shopList").innerHTML = resultHTML;
+}
+
+//お気に入りリスト作成
+function makeFavList(json, placeName, genreName) {
+  $("#shopList").empty();
+  var resultHTML = "<ul>";
+  var imgWidth = "200";
+  var imgHeight = "100";
+  for (var i = 0; i < json.length; i++) {
+    var rating = json[i].rating;
+    var shopName = json[i].shopName;
+    var genre = json[i].genre;
+    var place = json[i].place;
+    var imgURL = json[i].shopImgURL;
+    var id = json[i].id;
+    var check = json[i].checked;
+    var detailUrl =
+      "../html/store_detail.html" + "?" + "shopName" + "=" + shopName;
+    var star = makeStar(rating);
+
+    if (
+      (placeName === place || placeName === "") &&
+      (genreName === genre || genreName === "") &&
+      check == 1
+    ) {
+      //表示内容（評価＋名称）
+      var content =
+        "<img src=" +
+        imgURL +
+        " " +
+        "width=" +
+        imgWidth +
+        " " +
+        "height=" +
+        imgHeight +
+        " class=img" +
+        ">" +
+        "<br>" +
+        "<h3 class='shopName'>" +
+        shopName +
+        "</h3>" +
+        "<div class='rate'>" +
+        "<div class='star'>" +
+        star +
+        "</div>" +
+        rating +
+        "</div>" +
+        "<br>";
+
+      if (check == 0) {
+        var bookMark =
+          "<input type=" +
+          "checkbox" +
+          " " +
+          "id=" +
+          id +
+          " name=shop class=checkbox>";
+      } else {
+        var bookMark =
+          "<input type=" +
+          "checkbox" +
+          " " +
+          "id=" +
+          id +
+          " name=shop class=checkbox checked>";
+      }
+
+      resultHTML += '<li class="list">' + "<a href=" + detailUrl + ">";
+      resultHTML += content;
+      resultHTML +=
+        "<label class='check-box' for= " +
+        id +
+        ">" +
+        bookMark +
+        "<span class='check-text'>" +
+        "</span>" +
+        "</label>" +
+        "</a>";
+      resultHTML += "</li>";
+    }
+  }
+  resultHTML += "</ul>";
+  //結果表示
+  document.getElementById("shopList").innerHTML = resultHTML;
+}
+
+//ローカルストレージにデータあればロード　無ければアップする
+//JSONデータを返す
+function loadLocal() {
+  var data = localStorage.getItem("data");
+  if (data) {
+    console.log(data);
+    return JSON.parse(data);
+  } else {
+    $.getJSON("../json/shopData3.json").done(function (json) {
+      let json_data = JSON.stringify(json);
+      localStorage.setItem("data", json_data);
+      return json;
+    });
+  }
+}
+
+//お気に入りボタンを押したときJSONデータを変更し、ローカルストレージへアップロード
+$(function () {
+  $('input[name="shop"]').change(function () {
+    var json = loadLocal();
+    $('input[name="shop"]:checked').each(function () {
+      var r = $(this).attr("id");
+      json[r].checked = 1;
+    });
+    $('input[name="shop"]:not(:checked)').each(function () {
+      var r = $(this).attr("id");
+      json[r].checked = 0;
+    });
+    let json_data = JSON.stringify(json);
+    console.log(json_data);
+    localStorage.setItem("data", json_data);
+  });
+});
